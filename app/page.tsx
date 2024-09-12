6fc2c6336e3a4678a66acdaf5e48ed31d676ab10
Alejandro LR <alejandro.lr.developer@hotmail.com>
2024-09-12 11:59:56 -0500

    Initial Commit And added base ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,54 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="flex flex-col min-h-screen bg-[#355ADE]">
+      <main className="flex flex-col gap-4 flex-1 bg-white rounded-t-2xl mt-8 p-6">
+        {/* Courses List */}
+        <nav
+          className="flex flex-row gap-2 items-center text-white h-[60px] overflow-scroll"
+          role="navigation"
+        >
+          <button className="bg-[#355ADE] p-2 rounded-md hover:bg-[#2b4ed0]">
+            Matemática
+          </button>
+        </nav>
+        {/* Courses List */}
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
+        {/* Course Details */}
+        <section className="flex flex-col gap-4 flex-1 overflow-scroll">
+          {/* Course Details Header */}
+          <header>
+            <h1 className="text-3xl font-bold">Curso: Matemática</h1>
+            <h2 className="text-lg">
+              Profesor:{" "}
+              <a
+                className="text-[#355ADE] font-semibold hover:underline"
+                href="#"
+              >
+                Antonio Ruiz
+              </a>
+            </h2>
+            <h2>Código: LM-207</h2>
+          </header>
+          {/* Course Details Header */}
+
+          {/* Course Details Alumnos List */}
+          <div className="flex flex-col gap-2 h-96 overflow-scroll">
+            <div className="flex flex-row justify-between items-center bg-white border p-4 rounded-md">
+              <div className="flex flex-col">
+                <h2 className="font-semibold">Alejandro La Rosa</h2>
+                <p className="text-gray-400">
+                  Estudiante de Desarrollo de Software
+                </p>
+              </div>
+              <button className="bg-[#355ADE] px-4 py-1 text-white rounded-md hover:bg-[#2b4ed0]">
+                Ver
+              </button>
+            </div>
+          </div>
+          {/* Course Details Alumnos List */}
+        </section>
+        {/* Course Details */}
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
